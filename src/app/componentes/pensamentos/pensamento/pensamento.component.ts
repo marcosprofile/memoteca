ab4a9717a4c6df41a61f2a347213e5a5b25b1f19
Marcos Laurindo Ferreira <marcos_laurindo1997@hotmail.com>
2023-05-27 02:03:22 -0300

    Listando pensamentos

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+
+  @Input() pensamento = {
+    conteudo: 'I love Angular',
+    autoria: 'Marcos',
+    modelo: 'modelo3'
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
